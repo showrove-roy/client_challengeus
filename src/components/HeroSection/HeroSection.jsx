9e9b5327869b25c81e564cb06949ccd75bc67522
Showrove Roy <showrove200@gmail.com>
2023-05-28 01:11:15 +0600

    add time to hero section

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -3,13 +3,14 @@ import React from "react";
 const HeroSection = () => {
   return (
     <section
-      data-aos='fade-in-down'
-      data-aos-easing='ease-in-sine'
+      data-aos='zoom-in'
+      data-aos-easing='linear'
+      data-aos-duration='1000'
       className='heroBg h-screen flex justify-center items-center overflow-hidden'>
       <h1
         className='text-white text-7xl font-extrabold uppercase tracking-widest	text-center'
         data-aos='zoom-in-up'
-        data-aos-duration='900'>
+        data-aos-duration='1500'>
         Dare to
         <br />
         explore
